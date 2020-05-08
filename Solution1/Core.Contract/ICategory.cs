bd995a8d178de02e131d24a2a0d77e73896be34b
mahmoud <Mahmoudsabzalii@Gmail.com>
2020-05-08 08:36:36 -0700

    ADD TagHelper
    & Icategory

---
+++ Solution1/Core.Contract/ICategory.cs
@@ -0,0 +1,12 @@
+﻿using Core.Domian;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Contract
+{
+    public  interface ICategory:IRepository<Category>
+    {
+        
+    }
+}
