597813df46825f1de543dc691dfd31c054325a40
mahmoud <Mahmoudsabzalii@Gmail.com>
2020-05-21 15:23:00 -0700

    pay

--- Solution1/Service.AppService/payService.cs
+++ Solution1/Service.AppService/payService.cs
@@ -1,7 +1,10 @@
 ﻿using Core.Contract;
 using Core.Domian;
 using Microsoft.Extensions.Configuration;
-using System; 
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 
 namespace Service.AppService
 {
@@ -17,12 +20,36 @@ namespace Service.AppService
 
         public RequestPaymentResult RequestPayment(string amount, string mobile, string factorNumber, string description)
         {
-            throw new NotImplementedException();
+            HttpClient client = new HttpClient();
+            Dictionary<string, string> post_values = new Dictionary<string, string>();
+
+            post_values.Add("api", configuration["PayIr:ApiKey"]);
+            post_values.Add("amount", amount.ToString());
+            post_values.Add("redirect", configuration["PayIr:RedirectUrl"]);
+            post_values.Add("mobile", mobile);
+            post_values.Add("factorNumber", factorNumber);
+            post_values.Add("description", description);
+
+            var content = new FormUrlEncodedContent(post_values);
+            var response = client.PostAsync(configuration["PayIr:SendRequestUrl"], content).Result;
+
+            var responseString = response.Content.ReadAsStringAsync().Result;
+
+            return JsonConvert.DeserializeObject<RequestPaymentResult>(responseString);
+
+
         }
 
         public VerifyPayemtnResult VerifyPayment(string token)
         {
-            throw new NotImplementedException();
+            HttpClient client = new HttpClient();
+            Dictionary<string, string> post_values = new Dictionary<string, string>();
+            post_values.Add("api", configuration["PayIr:ApiKey"]);
+            post_values.Add("token", token);
+            var content = new FormUrlEncodedContent(post_values);
+            var response = client.PostAsync(configuration["PayIr:VerifyUrl"], content).Result;
+            var responseString = response.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<VerifyPayemtnResult>(responseString);
         }
     }
 }
